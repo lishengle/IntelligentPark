e8b0187d4f477cb13129a7714f8621c26b298abe
allen <1971059196@qq.com>
2018-03-10 15:09:20 +0800

    登录和注册页面

---
+++ app/src/main/java/com/example/intelligentpark/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.example.intelligentpark.constants;
+
+/**
+ * Created by 李盛乐 on 2018.3.10.
+ */
+
+public class Constants {
+}
---
+++ app/src/main/java/com/example/intelligentpark/pojo/Account.java
@@ -0,0 +1,8 @@
+package com.example.intelligentpark.pojo;
+
+/**
+ * Created by 李盛乐 on 2018.3.9.
+ */
+
+public class Account {
+}
